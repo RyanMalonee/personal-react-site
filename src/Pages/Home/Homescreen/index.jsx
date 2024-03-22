d4954dba9045f0d0a13a13455c875b9a14bc84ce
Ryan Malone <ryan.malone192@gmail.com>
2024-03-22 09:14:55 -0400

    Added all files

---
+++ src/Pages/Home/Homescreen/index.jsx
@@ -0,0 +1,12 @@
+import HeroSection from "../HeroSection";
+
+// This will be the home page and renders the components in the
+// order that they're listed
+export default function Homescreen() {
+  return (
+    // React Fragments
+    <>
+      <HeroSection />
+    </>
+  );
+}
