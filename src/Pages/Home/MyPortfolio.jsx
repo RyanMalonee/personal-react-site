cf72411bb510c9882216a6273bdbae15187fc01b
Ryan Malone <ryan.malone192@gmail.com>
2024-03-24 22:50:12 -0400

    progress

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -26,7 +26,7 @@ export default function MyPortfolio() {
         {data?.portfolio?.map((item, index) => (
           <div key={index} className="portfolio-section-card">
             <div className="portfolio-section-img">
-              <img src={item.src} alt="Placeholder" />
+              <img className="portfolio-img" src={item.src} alt="Placeholder" />
             </div>
             <div className="portfolio-section-card-content">
               <div>
