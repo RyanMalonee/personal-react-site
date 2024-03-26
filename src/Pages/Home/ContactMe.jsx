b2a39c371e4092f616e304eeec78c896a71387a4
Ryan Malone <ryan.malone192@gmail.com>
2024-03-25 22:20:19 -0400

    Added information and styling

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -31,7 +31,8 @@ export default function ContactMe() {
         <p className="sub-title">Get In Touch</p>
         <h2>Contact Me</h2>
         <p className="text-lg">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, odit.
+          Fill out the form below to get in touch with me. I'll get back to you
+          as soon I can!
         </p>
       </div>
       <form className="contact-form-container" onSubmit={onSubmit}>
